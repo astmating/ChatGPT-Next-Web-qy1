a42033b4e7aa91d4f772f3b05c1613f011a70ffb
astmating <131445482+astmating@users.noreply.github.com>
2023-05-18 18:45:57 +0800

    en.ts

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -36,6 +36,11 @@ const en: LocaleType = {
       SaveAs: "Save as Mask",
     },
   },
+    BootCamp: "AI集训营",
+    BootCamptext: {
+      text: "AI集训营",
+      putlink: "立即前往",
+    },
   Export: {
     Title: "All Messages",
     Copy: "Copy All",
